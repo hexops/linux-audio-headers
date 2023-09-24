ca57056b675e00751d457932b6ee49587e187162
Carl Åstholm <carl@astholm.se>
2023-09-24 20:48:29 +0200

    Pin upstream revs; Update headers

--- build.zig
+++ build.zig
@@ -6,16 +6,30 @@ pub fn build(b: *std.Build) void {
 
     const lib = b.addStaticLibrary(.{
         .name = "linux-audio-headers",
-        .root_source_file = .{ .path = "stub.c" },
+        .root_source_file = b.addWriteFiles().add("empty.c", ""),
         .target = target,
         .optimize = optimize,
     });
 
-    lib.installHeadersDirectory("alsa-lib", ".");
-    lib.installHeadersDirectory("jack", "jack");
-    lib.installHeadersDirectory("pipewire", "pipewire");
-    lib.installHeadersDirectory("pulse", "pulse");
-    lib.installHeadersDirectory("spa", "spa");
+    installHeadersDirectoryExcludeLicenseFiles(lib, "alsa-lib", ".");
+    installHeadersDirectoryExcludeLicenseFiles(lib, "jack", "jack");
+    installHeadersDirectoryExcludeLicenseFiles(lib, "pipewire", "pipewire");
+    installHeadersDirectoryExcludeLicenseFiles(lib, "pulse", "pulse");
+    installHeadersDirectoryExcludeLicenseFiles(lib, "sndio", ".");
+    installHeadersDirectoryExcludeLicenseFiles(lib, "spa", "spa");
 
     b.installArtifact(lib);
+}
+
+fn installHeadersDirectoryExcludeLicenseFiles(
+    lib: *std.build.Step.Compile,
+    src_dir_path: []const u8,
+    dest_rel_path: []const u8,
+) void {
+    lib.installHeadersDirectoryOptions(.{
+        .source_dir = .{ .path = src_dir_path },
+        .install_dir = .header,
+        .install_subdir = dest_rel_path,
+        .exclude_extensions = &.{ "COPYING", "LICENSE" },
+    });
 }
