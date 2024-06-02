c97923f8127344053069b56845df1b21bd2a3d12
Stephen Gutekanst <stephen@hexops.com>
2024-06-02 11:46:10 -0700

    update to Zig 2024.05
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -11,25 +11,12 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    installHeadersDirectoryExcludeLicenseFiles(lib, "alsa-lib", ".");
-    installHeadersDirectoryExcludeLicenseFiles(lib, "jack", "jack");
-    installHeadersDirectoryExcludeLicenseFiles(lib, "pipewire", "pipewire");
-    installHeadersDirectoryExcludeLicenseFiles(lib, "pulse", "pulse");
-    installHeadersDirectoryExcludeLicenseFiles(lib, "sndio", ".");
-    installHeadersDirectoryExcludeLicenseFiles(lib, "spa", "spa");
+    lib.installHeadersDirectory(b.path("alsa-lib"), ".", .{});
+    lib.installHeadersDirectory(b.path("jack"), "jack", .{});
+    lib.installHeadersDirectory(b.path("pipewire"), "pipewire", .{});
+    lib.installHeadersDirectory(b.path("pulse"), "pulse", .{});
+    lib.installHeadersDirectory(b.path("sndio"), ".", .{});
+    lib.installHeadersDirectory(b.path("spa"), "spa", .{});
 
     b.installArtifact(lib);
-}
-
-fn installHeadersDirectoryExcludeLicenseFiles(
-    lib: *std.Build.Step.Compile,
-    src_dir_path: []const u8,
-    dest_rel_path: []const u8,
-) void {
-    lib.installHeadersDirectoryOptions(.{
-        .source_dir = .{ .path = src_dir_path },
-        .install_dir = .header,
-        .install_subdir = dest_rel_path,
-        .exclude_extensions = &.{ "COPYING", "LICENSE" },
-    });
 }
